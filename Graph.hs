f49d6e110af481cb1d2685f0a00ad7a780eda154
Yitang Chen <yc4120@ic.ac.uk>
2021-01-24 13:57:39 +0800

    partially make class funcs of Graph for GraphList

--- Graph.hs
+++ Graph.hs
@@ -7,10 +7,20 @@ import           Control.Monad
 import           Control.Monad.Trans.State
 import           Data.Foldable
 import           Data.Maybe
-import           Prelude hiding (replicate)
+import           Prelude hiding (map, replicate)
 
 -- Requires installation
-import           Data.Sequence hiding (length, filter)
+import           Data.IntMap.Lazy 
+  (IntMap(..)
+  , adjust
+  , delete
+  , fromAscList
+  , insert
+  , map
+  , member
+  , (!)
+  )
+import           Data.Sequence hiding (adjust, filter, length)
 
 
 -- A type class for graphs
@@ -57,7 +67,7 @@ data GraphMatrix = MGraph
   , nodesM :: Seq Int
   , nodeMat :: Seq (Seq Int)
   }
-  -- deriving Show
+  deriving (Eq)
 
 instance Show GraphMatrix where
   show (MGraph _ nodes arcs)
@@ -77,9 +87,8 @@ instance Graph GraphMatrix where
       addArcs' m ((n, n') : as)
         = addArcs' (update nI row' m) as
         where
-          row' = update nI' (row `index` nI' + inc) row
+          row' = update nI' (row `index` nI' + 1) row
           row  = m `index` nI
-          inc  = if nI == nI' then 2 else 1
           nI   = fromJust $ elemIndexL n nodes
           nI'  = fromJust $ elemIndexL n' nodes
 
@@ -106,9 +115,8 @@ instance Graph GraphMatrix where
       removeArcs' m ((n, n') : as)
         = removeArcs' (update n row' m) as
         where
-          row' = update n' (max 0 $ row `index` n' - inc) row
+          row' = update n' (max 0 $ row `index` n' - 1) row
           row  = m `index` n
-          inc  = if n == n' then 2 else 1
 
   removeNodes [] g
     = g
@@ -134,6 +142,67 @@ instance Graph GraphMatrix where
     where
       overwrite = update (fromJust $ elemIndexL n nodes)
 
+
+-- Representing a graph as an adjacency list
+data GraphList = LGraph 
+  { nodeNumL :: Int
+  , nodeList :: IntMap (Seq Int)
+  }
+  deriving (Eq, Show)
+
+  -- TODO: Show instance
+
+instance Graph GraphList where
+  emptyGraph = LGraph 0 $ fromAscList []
+
+  addArcs arcs (LGraph size list)
+    = LGraph size $ addArcs' list arcs
+    where
+      addArcs' l []
+        = l
+      addArcs' l ((n, n') : as)
+        = addArcs' (adjust (insertAt 0 n') n l) as
+
+  addNodes [] g
+    = g
+  addNodes (n : ns) g@(LGraph size list)
+    | member n list = addNodes ns g
+    | otherwise     = addNodes ns $ LGraph (size + 1) (insert n empty list)
+
+  removeArcs arcs (LGraph size list)
+    = LGraph size $ removeArcs' list arcs
+    where
+      removeArcs' l []
+        = l
+      removeArcs' l ((n, n') : as)
+        | isNothing index = removeArcs' l as
+        | otherwise       = removeArcs' l' as
+        where
+          index = elemIndexL n' (l ! n)
+          l'    = adjust (deleteAt (fromJust index)) n l
+
+  removeNodes [] g
+    = g
+  removeNodes (n : ns) g@(LGraph size list)
+    | member n list = removeNodes ns $ LGraph (size - 1) l'
+    | otherwise     = removeNodes ns g
+    where
+      l' = map (execState removeAll) $ delete n list
+      removeEle i = state $ \s -> ((), deleteAt i s)
+      removeAll = do
+        entry <- get
+        let index = elemIndexL n entry
+        if (isNothing index)
+          then return ()
+          else do 
+            removeEle (fromJust index)
+            removeAll
+
+  -- TODO:
+  simplify = undefined
+  disconnect = undefined
+            
+
 -- Test graphs
 zrm, k3m, k4m, l4m :: GraphMatrix
 zrm = emptyGraph
@@ -141,3 +210,10 @@ k3m = initGraph [0..2] [(0, 1), (0, 2), (1, 0), (1, 2), (2, 0), (2, 1)]
 k4m 
   = addArcs [(0, 3), (1, 3), (2, 3), (3, 0), (3, 1), (3, 2)] $ addNodes [3] k3m
 l4m = initGraph [1..4] [(1, 2), (2, 3), (3, 4), (2, 1), (3, 2), (4, 3)]
+
+zrl, k3l, k4l, l4l :: GraphList
+zrl = emptyGraph
+k3l = initGraph [0..2] [(0, 1), (0, 2), (1, 0), (1, 2), (2, 0), (2, 1)]
+k4l 
+  = addArcs [(0, 3), (1, 3), (2, 3), (3, 0), (3, 1), (3, 2)] $ addNodes [3] k3l
+l4l = initGraph [1..4] [(1, 2), (2, 3), (3, 4), (2, 1), (3, 2), (4, 3)]
