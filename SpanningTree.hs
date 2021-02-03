3c491d195002b560ad1e0f87e24e9982dbe33f0c
Yitang Chen <yc4120@ic.ac.uk>
2021-02-03 23:52:29 +0800

    Fix Prim Algorithm Bug for not producing MST

--- SpanningTree.hs
+++ SpanningTree.hs
@@ -23,9 +23,9 @@ import           Utilities
 --------------------------------------------------------------------------------
 
 -- Pre: The graph is undirected and the given node is in the graph.
-primS :: Graph a => a -> Maybe a
-primS graph
-  | sz == 0      = Nothing
+prim :: Graph a => a -> Maybe a
+prim graph
+  | sz == 0      = Just emptyGraph
   | size k == sz = Just t
   | otherwise    = Nothing
   where
@@ -33,21 +33,22 @@ primS graph
     root        = head $ nodes graph
     initAdj     = execState (forM_ (neighbours root graph) $ \s -> do
       fringe <- get
-      put $ IM.insert s (root, fromJust (weight (root, s) graph)) fringe
+      put $ IM.insert s (fromJust (weight (root, s) graph), root) fringe
       ) empty
     ((k, _), t) = execState (loop_ $ do
       ((k, f), t) <- get
       breakWhen (null f) $ do
         let minN = minimumBy ((. (f !)) . compare . (f !)) (keys f)
-        let minA = let (n, w) = f ! minN in ((n, minN), w)
+        let minA = let (w, n) = f ! minN in ((n, minN), w)
         let adj  = neighbours minN graph
         let k'   = S.insert minN k
-        let fS   = breakWhen (size k' == sz) $ forM_ adj $ \s -> do
-            fringe <- get
-            continueWhen (S.member s k') $ do 
-              let newW = fromJust $ weight (minN, s) graph
-              put $ if IM.notMember s fringe
-                then IM.insert s (minN, newW) fringe
-                else IM.insert s (minN, min newW (snd $ fringe ! s)) fringe
-        put ((k', execState fS $ delete minN f), addUWArcs [minA] t)
-        ) ((fromDescList [root], initAdj), initUGraph (nodes graph) [])
+        put ((k', execState (breakWhen (size k' == sz) $ forM_ adj $ \s -> do
+          fringe <- get
+          let newW = fromJust $ weight (minN, s) graph
+          let new  = (newW, minN)
+          continueWhen (S.member s k') $ if IM.notMember s fringe
+            then put $ IM.insert s (newW, minN) fringe
+            else do
+              put $ IM.insert s (min (fringe ! s) new) fringe
+          ) $ delete minN f), addUWArcs [minA] t)
+      ) ((fromDescList [root], initAdj), initUGraph (nodes graph) [])
