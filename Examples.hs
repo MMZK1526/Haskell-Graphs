c44673cebc9b65631368e8317d06d58cb8bf7fb4
Yitang Chen <yc4120@ic.ac.uk>
2021-02-04 16:47:55 +0800

    Kruskal Examples

--- Examples.hs
+++ Examples.hs
@@ -222,7 +222,7 @@ k5BFSTree = breadthFirstTree 3 k5
 -------------------------------------------------------------------------------- 
 
 -- Examples of weighted graphs:
-wg1, wg2 :: GraphList
+wg1, wg2, wg3 :: GraphList
 wg1 
   = initUWGraph [1..4] 
   [((1, 2), 3), ((1, 3), 4), ((2, 4), 4), ((3, 4), 1), ((1, 4), 5)]
@@ -233,6 +233,13 @@ wg2
    ((5, 6), 9), ((5, 7), 8), ((6, 7), 7), ((6, 9), 4), ((6, 10), 5),
    ((7, 8), 10), ((7, 10), 9), ((8, 9), 4), ((8, 10), 3), ((9, 10), 1)
   ]
+wg3
+  = initUWGraph [1..8]
+  [((1, 2), 5), ((1,8), 7), ((2, 3), 6), ((2, 8), 3), ((2, 6), 8), ((3, 6), 1),
+   ((3, 4), 4), ((4, 5), 10), ((4, 6), 7), ((5, 6), 8), ((5, 7), 2), 
+  ((6, 7), 11), ((7, 8), 9)
+  ]
+
 
 -- Fiding Minimum Spanning Trees using Prim's Algorithm:
   -- primMST wg1
@@ -262,6 +269,20 @@ wg2
     -- Just 8
   -- primMSTWeights wg2
     -- Just 38
--- If the graph is not connected, returns Nothing
+-- If the graph is not connected, returns Nothing:
   -- primMST k3t2
     -- Nothing
+
+-- Fiding Minimum Spanning Trees using Kruskal's Algorithm:
+  -- kruskalMST wg3
+    -- 1: [2: 5]
+    -- 2: [1: 5, 3: 6, 8: 3]
+    -- 3: [2: 6, 4: 4, 6: 1]
+    -- 4: [3: 4]
+    -- 5: [6: 8, 7: 2]
+    -- 6: [3: 1, 5: 8]
+    -- 7: [5: 2]
+    -- 8: [2: 3]
+  -- kruskalMSTWeights wg3
+    -- Just 29
+  
