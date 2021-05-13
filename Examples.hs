5d69d9d6c94021695075bf4438bda8837568ced6
Yitang Chen <yc4120@ic.ac.uk>
2021-05-13 16:40:20 +0800

    For Exam

--- Examples.hs
+++ Examples.hs
@@ -455,3 +455,21 @@ mergeSorted = mergeSort arr
 
 heapSorted :: Vec1D Int
 heapSorted = heapSort arr
+
+
+-- For Exam
+a, b, c, d, e, f, g, h, i, j, k, l, m, n :: Int
+a = 1
+b = 2
+c = 3
+d = 4
+e = 5
+f = 6
+g = 7
+h = 8
+i = 9
+j = 10
+k = 11
+l = 12
+m = 13
+n = 14
